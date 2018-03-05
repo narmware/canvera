bfb7e678328f22ae613dc751d8d11d2ff5a93764
Rohit Savant <savantrohit8@gmail.com>
2018-03-05 15:43:42 +0530

    Cleaned splash ui

---
+++ app/src/main/java/com/narmware/canvera/MyApplication.java
@@ -0,0 +1,14 @@
+package com.narmware.canvera;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+/**
+ * Created by rohitsavant on 05/03/18.
+ */
+
+@Singleton
+public class MyApplication extends Application {
+    
+}
