84a2320f04b75ad9033b61d1fb0e3a9b2bb640fa
Rohit Savant <savantrohit8@gmail.com>
2018-03-06 11:48:29 +0530

    Login UI

--- app/src/main/java/com/narmware/canvera/activity/LoginActivity.java
+++ app/src/main/java/com/narmware/canvera/activity/LoginActivity.java
@@ -1,30 +1,23 @@
 package com.narmware.canvera.activity;
 
-import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.FrameLayout;
 
 import com.narmware.canvera.R;
 import com.narmware.canvera.databinding.ActivityLoginBinding;
 import com.narmware.canvera.fragment.LoginFragment;
+import com.narmware.canvera.fragment.SignInFragment;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class LoginActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+public class LoginActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, SignInFragment.OnFragmentInteractionListener {
     protected ActivityLoginBinding mBinding;
     protected FragmentManager mFragmentManager;
-    @BindView(R.id.login_fragment_container) protected FrameLayout mFragmentContainer;
 
     private void init() {
         mFragmentManager = getSupportFragmentManager();
@@ -38,13 +31,13 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
         init();
 
         FragmentTransaction transaction = mFragmentManager.beginTransaction();
-        transaction.add(R.id.login_fragment_container, new LoginFragment());
+        transaction.add(R.id.login_fragment_container, new SignInFragment());
         transaction.commit();
 
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-        
+
     }
 }
