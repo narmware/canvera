53d68f8cc28a9e4dde39cceb634b95151ebc6f40
Vrushali Varne <vrushali@narmware.com>
2018-03-22 11:21:09 +0530

    Added date picker to book appointment

--- app/src/main/java/com/narmware/canvera/activity/BookAppointActivity.java
+++ app/src/main/java/com/narmware/canvera/activity/BookAppointActivity.java
@@ -1,14 +1,39 @@
 package com.narmware.canvera.activity;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.narmware.canvera.MyApplication;
 import com.narmware.canvera.R;
+import com.narmware.canvera.helpers.Constants;
+import com.narmware.canvera.helpers.SupportFunctions;
+import com.narmware.canvera.support.customfonts.MyEditText;
+
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BookAppointActivity extends AppCompatActivity {
+    @BindView(R.id.edt_start_date)protected MyEditText mEdtStartDate;
+    RequestQueue mVolleyRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +41,7 @@ public class BookAppointActivity extends AppCompatActivity {
         setContentView(R.layout.activity_book_appoint);
         getSupportActionBar().hide();
 
+        init();
        /* if(getCurrentFocus()!=null) {
 
 
@@ -23,4 +49,91 @@ public class BookAppointActivity extends AppCompatActivity {
             imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
         }*/
     }
+
+    private void init() {
+        ButterKnife.bind(this);
+        mVolleyRequest = Volley.newRequestQueue(this);
+
+        mEdtStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(0);
+            }
+        });
+
+    }
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        Date c = Calendar.getInstance().getTime();
+        int year=c.getYear();
+        int month=c.getMonth();
+        int day=c.getDay();
+        return new DatePickerDialog(this, datePickerListener,2018,month, day);
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+           /* day = selectedDay;
+            month = selectedMonth;
+            year = selectedYear;*/
+            Toast.makeText(BookAppointActivity.this,selectedDay + " / " + (selectedMonth + 1) + " / "
+                    + selectedYear,Toast.LENGTH_SHORT).show();
+            mEdtStartDate.setText(selectedDay + " / " + (selectedMonth + 1) + " / " + selectedYear);
+        }
+    };
+
+
+    private void setEvent(String isFirst,String type) {
+      /*  final ProgressDialog dialog = new ProgressDialog(getContext());
+        dialog.setMessage("getting images ...");
+        dialog.setCancelable(false);
+        dialog.show();*/
+
+        HashMap<String,String> param = new HashMap();
+        param.put(Constants.IS_FIRST,isFirst);
+        param.put(Constants.TOP_TYPE,type);
+
+        String url= SupportFunctions.appendParam(MyApplication.URL_FEATURED_IMGS,param);
+
+        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET,url,null,
+                // The third parameter Listener overrides the method onResponse() and passes
+                //JSONObject as a parameter
+                new Response.Listener<JSONObject>() {
+
+                    // Takes the response from the JSON request
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try
+                        {
+                            //getting test master array
+                            Log.e("Json_string",response.toString());
+                            Gson gson = new Gson();
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            //  dialog.dismiss();
+                        }
+                        //dialog.dismiss();
+                    }
+                },
+                // The final parameter overrides the method onErrorResponse() and passes VolleyError
+                //as a parameter
+                new Response.ErrorListener() {
+                    @Override
+                    // Handles errors that occur due to Volley
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", "Test Error");
+                        //dialog.dismiss();
+
+                    }
+                }
+        );
+        mVolleyRequest.add(obreq);
+    }
+
 }
